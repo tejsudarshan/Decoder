42fa14399ca8aadf6521cac47a38716bed47a3a3
Tejas Sudarshan <59815659+tejsudarshan@users.noreply.github.com>
2022-04-04 21:30:54 +0530

    Create test.v

---
+++ test.v
@@ -0,0 +1,15 @@
+module test;
+reg [2:0]in;
+wire [7:0]out;
+Decoder DUT(.in(in),.out(out));
+initial begin
+$monitor($time,"in=%b out2=%b",in,out);
+in=3'b000;
+#5 in=3'b001;
+#5 in=3'b011;
+#5 in=3'b101;
+#5 in=3'b110;
+#5 in=3'b111;
+#5;
+end
+endmodule
